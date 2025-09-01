30665b78e5ad2d23faf95bad6a8fe0dc0a00240d
Arthur Ryzhak <ryzhakar@gmail.com>
2025-09-02 00:04:33 +0300

    refactor: delete most junk

--- src/scripts/base.js
+++ src/scripts/base.js
@@ -1,23 +1,5 @@
 'use strict';
 
-$(document).ready(function() {
-
-    // Smooth scrolling
-    $('a[href*="#"]:not([href="#"])').click(function() {
-        if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-            if (target.length) {
-                $('html,body').animate({
-                    scrollTop: target.offset().top
-                }, 1000);
-                return false;
-            }
-        }
-    });
-
-});
-
 (function() {
     var files = Array();
     var queue = Array();
